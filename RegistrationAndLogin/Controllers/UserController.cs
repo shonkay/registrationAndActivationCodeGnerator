0752cba0b5f26e462204eb326d49a71a7178f6f5
Shontan Oluwaseun <shonkayworld_don@yahoo.com>
2018-11-22 09:53:38 +0100

    Importing Project

---
+++ RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //Registration POST action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude ="IsEmailVerified, ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            //Modelvalidation
+            if (ModelState.IsValid)
+            {
+            
+                #region //Email is already Exist or not
+                var isExist = IsEmailExist(user.EmailID);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exist");
+                    return View(user);
+                }
+                #endregion
+
+                #region Generate Activation Code
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                #endregion
+                user.IsEmailVerified = false;
+
+                #region  Sava data to Database
+                using(MyDataBaseEntities dc = new MyDataBaseEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    //Send Email to User
+                    sendVerificationLinkEmail(user.EmailID, user.ActivationCode.ToString());
+                    message = "Registration successfully done. Account activation link has been sent" +
+                        " to your email id:" + user.EmailID;
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        //verify Email
+
+        //verify Email LINK
+
+        //login
+
+        //login POST
+
+        //logout
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (MyDataBaseEntities dc = new MyDataBaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).ToList();
+                return v.Count>0 ? true : false;
+            }
+        }
+
+        [NonAction]
+        public void sendVerificationLinkEmail(string emailID, string activationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("shonkayworld1234don@gmail.com", "Kayode(Software Developer)SBSC"); 
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "1234oluwaseun"; // To be replaced with actual password
+            string subject = "Your account is successfully created!";
+
+            string body = "<br/><br/> Your account has been successfully created. Please click on the " +
+                "below link to verify your account" + "<br/><br/> <a href = '" + link + "'>" + link + "<a/>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+    }
+}
---
+++ RegistrationAndLogin/Models/Extended/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationAndLogin.Models
+{
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+    public class UserMetaData
+    {
+        [Display(Name =" First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = " First name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name =" Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = " Last name required")]
+        public string LastName { get; set; }
+
+        [Display(Name =" EmailID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = " Email ID required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+
+        [Display(Name =" Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataType(DataType.Password)]
+        [MinLength(8,ErrorMessage =" Minimum of 8 characters required")]
+        [Required(AllowEmptyStrings = false, ErrorMessage =" Password is required")]
+        public string Password { get; set; }
+
+        [Display(Name =" Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage =" Confirm Password and Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
